34a4d4194b8fcd966acafa5104f2d3365372a50e
Chetan Gandhi <69512667+Chetan2808@users.noreply.github.com>
2023-07-18 23:09:57 +0530

    find odd or even from given number

---
+++ find_odd_or_even.py
@@ -0,0 +1,7 @@
+number=int(input("Enter your number: "))
+if(number==0):
+    print("Your number is zero")
+elif (number%2==0):
+    print("Your number is even")
+else:
+    print("Your number is odd")
